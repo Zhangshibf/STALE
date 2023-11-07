375539190264d7ef7e73c22bfdd31a5f53e55762
zhangshibf <zhangshibf@gmail.com>
2023-11-07 11:57:30 +0100

    a

--- config/zero_shot.py
+++ config/zero_shot.py
@@ -47,8 +47,8 @@ t1_dict_test = {split_t1_test[i] : i for i in sorted(range(50))}
 
 # {'Baton twirling': 0, 'Beach soccer': 1, 'Blowing leaves': 2, 'Bullfighting': 3, 'Capoeira': 4, 'Croquet': 5, 'Disc dog': 6, 'Drum corps': 7, 'Fixing the roof': 8, 'Having an ice cream': 9, 'Kneeling': 10, 'Making a lemonade': 11, 'Playing beach volleyball': 12, 'Playing blackjack': 13, 'Rafting': 14, 'Removing ice from car': 15, 'Swimming': 16, 'Trimming branches or hedges': 17, 'Tug of war': 18, 'Using the monkey bar': 19, 'Waterskiing': 20, 'Welding': 21, 'Drinking coffee': 22, 'Zumba': 23, 'High jump': 24, 'Wrapping presents': 25, 'Cricket': 26, 'Preparing pasta': 27, 'Grooming horse': 28, 'Preparing salad': 29, 'Playing polo': 30, 'Long jump': 31, 'Tennis serve with ball bouncing': 32, 'Layup drill in basketball': 33, 'Cleaning shoes': 34, 'Shot put': 35, 'Fixing bicycle': 36, 'Using parallel bars': 37, 'Playing lacrosse': 38, 'Cumbia': 39, 'Tai chi': 40, 'Mowing the lawn': 41, 'Walking the dog': 42, 'Playing violin': 43, 'Breakdancing': 44, 'Windsurfing': 45, 'Removing curlers': 46, 'Archery': 47, 'Polishing forniture': 48, 'Playing badminton': 49}
 
-t2_dict_train = {split_t2_train[i] : i for i in sorted(range(100))}
-t2_dict_test = {split_t2_test[i] : i for i in sorted(range(100))}
+t2_dict_train = {split_t2_train[i] : i for i in sorted(range(10))}
+t2_dict_test = {split_t2_test[i] : i for i in sorted(range(10))}
 
 # for i in range(100)
 #     split_t1_dict[i] = {
