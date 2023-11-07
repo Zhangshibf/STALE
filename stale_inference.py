9b6a871c2fb2856162db4097ec6d10247328d700
zhangshibf <zhangshibf@gmail.com>
2023-11-07 10:30:04 +0100

    a

--- stale_inference.py
+++ stale_inference.py
@@ -43,7 +43,15 @@ if __name__ == '__main__':
     model = torch.nn.DataParallel(model, device_ids=[0]).cuda()
     ### Load Checkpoint ###
     checkpoint = torch.load("/home/CE/zhangshi/checkpoint/STALE_best_50_split.pth.tar")
-    model.load_state_dict(checkpoint['state_dict'])
+    new_state_dict={}
+    for k, v in checkpoint['state_dict'].items():
+        if k in [ "module.txt_model.text_model.embeddings.position_ids", "module.txt_model.vision_model.embeddings.position_ids"]:
+            new_state_dict[k.replace("module.", "")]=v
+        else:
+            new_state_dict[k]=v
+
+    #model.load_state_dict(checkpoint['state_dict'])
+    model.load_state_dict(new_state_dict)
     model.eval()
     ### Load Dataloader ###
     test_loader = torch.utils.data.DataLoader(stale_dataset.STALEDataset(subset="test", mode='inference'),
