96a29473472cea3cf66c3a54168287dbc252847f
zhangshibf <zhangshibf@gmail.com>
2023-11-09 16:39:12 +0100

    a

--- config/dataset_class.py
+++ config/dataset_class.py
@@ -1,3 +1,2 @@
-activity_dict={'BaseballPitch': 0, 'CleanAndJerk': 1, 'CricketShot': 2, 'Diving': 3, 'HighJump': 4, 'PoleVault': 5, 'Shotput': 6, 'TennisSwing': 7, 'ThrowDiscus': 8, 'VolleyballSpiking': 9}
-
+activity_dict={'CricketBowling': 0, 'FrisbeeCatch': 1, 'GolfSwing': 2, 'HammerThrow': 3, 'LongJump': 4, 'PoleVault': 5, 'Shotput': 6, 'SoccerPenalty': 7, 'TennisSwing': 8, 'VolleyballSpiking': 9}
 
