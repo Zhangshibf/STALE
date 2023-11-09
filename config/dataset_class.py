9be223482174202f6f2d953cafdf7892928c2e9d
zhangshibf <zhangshibf@gmail.com>
2023-11-09 16:16:07 +0100

    a

--- config/dataset_class.py
+++ config/dataset_class.py
@@ -1,3 +1,6 @@
 bla= {"BaseballPitch" : 0, "BasketballDunk" : 1 , "Billiards" : 2, "CleanAndJerk" : 3, "CliffDiving" : 4, "CricketBowling" : 5, "CricketShot" : 6, "Diving" : 7, "FrisbeeCatch" : 8, "GolfSwing" : 9, "HammerThrow" : 10, "HighJump" : 11, "JavelinThrow" : 12, "LongJump" : 13, "PoleVault" : 14, "Shotput" : 15,"SoccerPenalty" : 16, "TennisSwing" : 17,"ThrowDiscus" : 18, "VolleyballSpiking" : 19, "Ambiguous" : 20}
 activity_dict={'BaseballPitch': 0, 'Billiards': 1, 'CleanAndJerk': 2, 'GolfSwing': 3, 'HammerThrow': 4, 'HighJump': 5, 'PoleVault': 6, 'SoccerPenalty': 7, 'TennisSwing': 8, 'ThrowDiscus': 9}
+
+
+
 aa=  {"Baseball" : 0, "Basketball" : 1 , "Billiards" : 2, "CleanAndJerk" : 3, "CliffDiving" : 4, "Bowling" : 5, "Shot" : 6, "Diving" : 7, "Frisbee" : 8, "Golf" : 9, "HammerThrow" : 10, "HighJump" : 11, "JavelinThrow" : 12, "LongJump" : 13, "PoleVault" : 14, "Shotput" : 15,"Soccer" : 16, "Tennis" : 17,"Discus" : 18, "Volleyball" : 19, "Ambiguous" : 20}
